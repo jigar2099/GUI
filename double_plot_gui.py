e1185bf66a4074c9978d72b84946ecd35dd62501
jigar bhanderi <41480945+jigar2099@users.noreply.github.com>
2022-03-31 23:28:42 +0200

    Update double_plot_gui.py

--- double_plot_gui.py
+++ double_plot_gui.py
@@ -14,25 +14,120 @@ import matplotlib.animation as animation
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+from tkinter import filedialog
+from clsNum import closestNumber as clsnum
+
 
 LARGE_FONT= ("Verdana", 12)
 style.use("ggplot")
 
 
-
 f = Figure(figsize=(5,5), dpi=100)
-a = f.add_subplot(111)
+a = f.add_subplot(1,1,1)
 def animate(i):
     data = pd.read_csv('data.csv')
+    steps = len(data)
+    samp_size = 180
+    c_num = clsnum(steps,samp_size)
+    
+    def plotting(samp_size,arr_len):
+        x = np.array(data['x_value'])[:arr_len]
+        #print(x.shape[0])
+        x_mean = x.reshape(int(x.shape[0]/samp_size),samp_size)
+        x_m = np.mean(x_mean,axis=1)
+
+        y1 = np.array(data['total_1'])[:arr_len]
+        y1_mean = y1.reshape(int(y1.shape[0]/samp_size),samp_size)
+        y1_m = np.mean(y1_mean,axis=1)
+        y1_s = np.std(y1_mean,axis=1)
+
+        y2 = np.array(data['total_2'])[:arr_len]
+        y2_mean = y2.reshape(int(y2.shape[0]/samp_size),samp_size)
+        y2_m = np.mean(y2_mean,axis=1)
+        y2_s = np.std(y2_mean,axis=1)
+        a.clear()
+        a.plot(x_m,y1_m,label='channel-1',color='green')
+        a.errorbar(x_m,y1_m,yerr=y1_s,color='green',capsize=3,elinewidth=1)
+        a.plot(x_m,y2_m,label='channel-2',color='blue') 
+        a.errorbar(x_m,y2_m,yerr=y2_s,color='blue',capsize=3,elinewidth=1)
+        threshold = 1000
+        # Add below threshold markers
+        below_threshold = y1_m < threshold
+        a.scatter(x_m[below_threshold], y1_m[below_threshold], color='red',s=45)
+        
+        # Add above threshold markers
+        above_threshold = np.logical_not(below_threshold)
+        a.scatter(x_m[above_threshold], y1_m[above_threshold],color='green')
+        
+        # Add below threshold markers
+        below_threshold = y2_m < threshold
+        a.scatter(x_m[below_threshold], y2_m[below_threshold], color='red',s=45)
+        # Add above threshold markers
+        above_threshold = np.logical_not(below_threshold)
+        a.scatter(x_m[above_threshold], y2_m[above_threshold],color='blue')        
+        
+        
+        a.fill_between(x_m, 0, 1000, facecolor='yellow', alpha=0.5,
+                label='Threshold')
+        if min(y1)<min(y2) and max(y2)<max(y1):
+            a.set_ylim(min(y1),max(y1))
+        elif min(y1)<min(y2) and max(y1)<max(y2):
+            a.set_ylim(min(y1),max(y2))
+        elif min(y2)<min(y1) and max(y2)<max(y1):
+            a.set_ylim(min(y2),max(y1))
+        elif min(y2)<min(y1) and max(y1)<max(y2):
+            a.set_ylim(min(y2),max(y2))
+            
+        a.set_xlabel('time')
+        a.set_ylabel('count')
+        a.legend(bbox_to_anchor=(0, 1.02, 1, .102), loc=3,
+                 ncol=2, borderaxespad=0)
+        #a.tight_layout()
+
+    
+    
+    #print(steps,c_num)
+    if steps%samp_size==0:
+        if c_num>steps:
+            #print("Go back ! ",c_num-samp_size)
+            arr_len = c_num-samp_size
+            plotting(samp_size,arr_len)
+        elif c_num==0:
+            pass
+        else:
+            #print("Already perfect ! ",c_num)
+            arr_len = c_num
+            plotting(samp_size,arr_len)
+    else:
+        pass
+
+
+
+
+
+f1 = Figure(figsize=(5,5), dpi=100)
+a1 = f1.add_subplot(1,1,1)
+def animate1(i):
+    data = pd.read_csv('data.csv')
     x = np.array(data['x_value'])
+    
     y1 = np.array(data['total_1'])#[:arr_len]
+    #y1_mean = y1.reshape(int(y1.shape[0]/samp_size),samp_size)
+    #y1_m = np.mean(y1_mean,axis=1)
+    #y1_s = np.std(y1_mean,axis=1)
+
     y2 = np.array(data['total_2'])#[:arr_len]
-    a.clear()
-    a.plot(x, y1,label='channel-1')
-    a.plot(x, y2,label='channel-2')
-    a.set_xlabel('time')
-    a.set_ylabel('count')
-    a.legend(bbox_to_anchor=(0, 1.02, 1, .102), loc=3,
+    #y2_mean = y2.reshape(int(y2.shape[0]/samp_size),samp_size)
+    #y2_m = np.mean(y2_mean,axis=1)
+    #y2_s = np.std(y2_mean,axis=1)
+    
+    a1.clear()
+    a1.plot(x, y1, label='channel-1',color='green')
+    a1.plot(x, y2 , label='channel-2',color='blue')
+    a1.set_xlabel('time')
+    a1.set_ylabel('count')
+    a1.legend(bbox_to_anchor=(0, 1.02, 1, .102), loc=3,
              ncol=2, borderaxespad=0)
 
 class LivePlot(tk.Tk):
@@ -60,11 +155,12 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="""This GUI is created for live plotting only.""", font=LARGE_FONT)
+        label = tk.Label(self, text="""This Std plotting.""", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         button1 = ttk.Button(self, text="Visualize",
                             command=lambda: controller.show_frame(Plot_page))
         button1.pack()
+        
         button2 = ttk.Button(self, text="Quit",
                             command=quit)
         button2.pack()
@@ -94,7 +190,13 @@ class Plot_page(tk.Frame):
         toolbar = NavigationToolbar2Tk(canvas, self)
         toolbar.update()
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        
+        canvas1 = FigureCanvasTkAgg(f1, self)
+        canvas1.draw()
+        canvas1.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        toolbar = NavigationToolbar2Tk(canvas1, self)
+        canvas1._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 app = LivePlot()
 ani = animation.FuncAnimation(f, animate, interval=1000)
+ani1 = animation.FuncAnimation(f1, animate1, interval=1000)
 app.mainloop()
-        
